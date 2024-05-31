a38fb9d9487dca36558a97a3e656b948fc294ffd
luongthtai <luongthetai2705@gmail.com>
2024-05-31 10:15:07 +0700

    add ham

--- src/app/controllers/reactController/test6.js
+++ src/app/controllers/reactController/test6.js
@@ -1,3 +1,10 @@
-const fuc = () => {
+const fuceeeeee = () => {
+    console.log("hello")
+    setTimeout(() => {
+        console.log("fdtdtdr")
+    }, 5000)
+}
+
+const func = () => {
     console.log("hello")
 }
